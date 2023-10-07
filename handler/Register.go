d3e8eda6aafa56dc7f4b8f9e2726b5f796b94c31
danimunandar <danyoey38@gmail.com>
2023-10-07 19:28:27 +0700

    update Handler regiter dan login

--- handler/Register.go
+++ handler/Register.go
@@ -7,21 +7,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// Register digunakan untuk mendaftarkan pelanggan ke dalam database
-// func Register(customer entity.Customer) error {
-// 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(customer.Password), bcrypt.DefaultCost)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	_, err = config.DB.Exec("INSERT INTO customers (username, password) VALUES (?,?)", customer.Username, hashedPassword)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
 func Register(customer entity.Customer) error {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(customer.Password), bcrypt.DefaultCost)
 	if err != nil {
